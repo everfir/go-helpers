a8790d6279763d46d347bb90924c66988c74c157
lpf11 <laipeifeng@everfir.com>
2025-02-06 14:07:20 +0800

    improve business filter

--- middleware/business_middleware.go
+++ middleware/business_middleware.go
@@ -24,30 +24,20 @@ var getBusinessConfig func() *define.Config[structs.BusinessConfig] = sync.OnceV
 func BusinessMiddleware(c *gin.Context) {
 	// 根据header中的字段来确定业务
 	business := c.GetHeader(env.BusinessKey.String())
-	if business == "" {
+	valid := getBusinessConfig().Get().Valid(business)
+	if !valid {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"err_code": http.StatusBadRequest,
-			"err_msg":  "unexcept business field in header",
+			"err_msg":  "business field in header is not expected, this business does not exist",
 		})
 		return
 	}
 
-	// 只有配置中存在的business才可正确操作
-	businessList := getBusinessConfig().Get().BusinessList
-	for _, b := range businessList {
-		if b.Name == business && b.Status == 1 {
-			ctx := c.Request.Context()
-			ctx = context.WithValue(ctx, env.BusinessKey, business)
-			c.Set(env.BusinessKey.String(), business)
-			c.Request = c.Request.WithContext(ctx)
+	ctx := c.Request.Context()
+	ctx = context.WithValue(ctx, env.BusinessKey, business)
+	c.Set(env.BusinessKey.String(), business)
+	c.Request = c.Request.WithContext(ctx)
 
-			c.Next()
-		}
-	}
+	c.Next()
 
-	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-		"err_code": http.StatusBadRequest,
-		"err_msg":  "business field in header is not expected, this business does not exist.",
-	})
-	return
 }
