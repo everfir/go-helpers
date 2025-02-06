63a5f585565b353e0e9505462c1862ee1fb45c89
lpf11 <laipeifeng@everfir.com>
2025-02-06 14:07:20 +0800

    improve business filter

--- internal/structs/business.go
+++ internal/structs/business.go
@@ -1,13 +1,36 @@
 package structs
 
+type BusinessStatus uint8
+
+const (
+	BUSINESS_STATUS_OFFLINE BusinessStatus = 0
+	BUSINESS_STATUS_OK      BusinessStatus = 1
+)
+
 type BusinessConfig struct {
 	BusinessList []Business `json:"business"`
+	businessMap  map[string]Business
 }
 
 type Business struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Ctime       string `json:"ctime"`
-	Status      uint8  `json:"status"`
+	Id          string         `json:"id"`
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	Ctime       string         `json:"ctime"`
+	Status      BusinessStatus `json:"status"`
+}
+
+func (b *BusinessConfig) Format() {
+	b.businessMap = make(map[string]Business)
+	for _, business := range b.BusinessList {
+		b.businessMap[business.Name] = business
+	}
+}
+
+func (b BusinessConfig) Valid(business string) bool {
+	if business == "" {
+		return false
+	}
+	config, exist := b.businessMap[business]
+	return exist && config.Status == BUSINESS_STATUS_OK
 }
